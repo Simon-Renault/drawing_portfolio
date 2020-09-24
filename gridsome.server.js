4f0d3e0482fcf34164c2a9dc7207a1e88d98180c
simon <simon.renault.pro@gmail.com>
2020-09-24 17:52:47 +0100

    test

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,54 +1,54 @@
-const homeResources = require( "./config/home_config.json")
+// import homeResources from  "/config/home_config.json"
 
-module.exports = (api) => {
-    api.loadSource((actions) => {
+// module.exports = (api) => {
+//     api.loadSource((actions) => {
 
-        const ResourceType = actions.schema.createUnionType({
-            name: "Resource",
-            types: ["Quote", "Item"],
-            resolveType: (obj) => obj.component
-        });
+//         const ResourceType = actions.schema.createUnionType({
+//             name: "Resource",
+//             types: ["Quote", "Item"],
+//             resolveType: (obj) => obj.component
+//         });
     
-        const QuoteType = actions.schema.createObjectType({
-            name: "Quote",
-            interfaces: ["Node"],
-            fields: {
-            id: "ID!",
-            quote: "String",
-            component: "String!"
-            }
-        });
+//         const QuoteType = actions.schema.createObjectType({
+//             name: "Quote",
+//             interfaces: ["Node"],
+//             fields: {
+//             id: "ID!",
+//             quote: "String",
+//             component: "String!"
+//             }
+//         });
     
 
-        actions.addSchemaTypes([ResourceType, QuoteType]);
+//         actions.addSchemaTypes([ResourceType, QuoteType]);
     
-        const pages = actions.addCollection("HomePage");
-        const quoteCollection = actions.addCollection("Quote");
-        const Items = actions.getCollection("Item").data()
+//         const pages = actions.addCollection("HomePage");
+//         const quoteCollection = actions.addCollection("Quote");
+//         const Items = actions.getCollection("Item").data()
 
-        let refs = [];
+//         let refs = [];
 
-        homeResources.items.map((item,index) => {
-                if(item.template === "quote"){
-                    const node = quoteCollection.addNode({
-                        quote : item.quote,
-                        component : "Quote"
-                    })
-                    refs.push(actions.createReference(node))
-                }
-                if(item.template === "home-page-item"){
-                    const path = '/' + item.item_referance.split('/')[1].split('.')[0] + '/'
-                    const Item = Items.find(i => i.path === path)
-                    refs.push({typeName: 'Item', id: Item.id})
-                }
-        })
+//         homeResources.items.map((item,index) => {
+//                 if(item.template === "quote"){
+//                     const node = quoteCollection.addNode({
+//                         quote : item.quote,
+//                         component : "Quote"
+//                     })
+//                     refs.push(actions.createReference(node))
+//                 }
+//                 if(item.template === "home-page-item"){
+//                     const path = '/' + item.item_referance.split('/')[1].split('.')[0] + '/'
+//                     const Item = Items.find(i => i.path === path)
+//                     refs.push({typeName: 'Item', id: Item.id})
+//                 }
+//         })
 
-        pages.addNode({
-            name: "Home",
-            resources: refs
-        });
+//         pages.addNode({
+//             name: "Home",
+//             resources: refs
+//         });
 
 
-    });
-  };
+//     });
+//   };
   
